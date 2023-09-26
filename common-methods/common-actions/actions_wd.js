064931a33da7521a52cb2d8b6007e8dd8adb523b
Siva Kolli <skolli@morneaushepell.com>
2023-09-26 10:32:52 +0530

    updates

--- common-methods/common-actions/actions_wd.js
+++ common-methods/common-actions/actions_wd.js
@@ -4,6 +4,10 @@ class Actions {
     async click_element(element){
         await element.click();
     }
+
+    async type_value(element, data){
+        await element.setValue(data);
+    }
 }
 
 module.exports = new Actions();
--- pages/sauce-po/sauce_demo.js
+++ pages/sauce-po/sauce_demo.js
@@ -5,27 +5,17 @@ class SauceDemo {
      * define selectors using getter methods
      */
   
-    get username_inputbox() {
-      return $("#user-name");
-    }
-    get password_inputbox() {
-      return $("#password");
-    }
-    get submit_button() {
-      return $(".submit-button.btn_action");
-    }
-    get btnSubmit() {
-      return $("a[aria-label='Search']");
-    }
+    get username_inputbox() { return $("#user-name");}
+    get password_inputbox() { return $("#password");}
+    get submit_button() { return $(".submit-button.btn_action");}
+    get btnSubmit() { return $("a[aria-label='Search']");}
 
   async login(username, password) {
-    await this.username_inputbox.setValue(username);
-    await this.password_inputbox.setValue(password);
-    //await this.submit_button.click();
-    await Actions.click_element(this.submit_button)
+    await Actions.type_value(this.username_inputbox, username);
+    await Actions.type_value(this.password_inputbox, password);
+    await Actions.click_element(this.submit_button);
   }
 
-   
   }
   module.exports = new SauceDemo();
   
